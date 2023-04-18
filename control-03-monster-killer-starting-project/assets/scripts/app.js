523e0d4b99fc50fd18c17ee35571a7bad4ccdc73
Renan Amorim <renanamorim89@hotmail.com>
2023-04-18 13:58:39 -0300

    Adding an "Attack" Function

---
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -0,0 +1,14 @@
+const ATTACK_VALUE = 10;
+
+let chosenMaxLife = 100;
+let currentMonsterHelth = chosenMaxLife;
+let currentPlayerHealyh = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHelth -= damage;
+}
+
+attackBtn.addEventListener("click", attackHandler);
